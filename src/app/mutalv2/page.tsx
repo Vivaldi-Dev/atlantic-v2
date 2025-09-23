2a23ec75a35bb01cf72ffe711599bef6b819ae1b
Vivaldi Dev <nobelavivaldi@gmail.com>
2025-09-24 00:11:49 +0200

    add new styles

--- src/app/mutalv2/page.tsx
+++ src/app/mutalv2/page.tsx
@@ -76,165 +76,195 @@ const NestedDiamond = () => {
 
 
         <div className='overflow-hidden'>
-            <div className=" bg-red-100 h-[600px] mt-20 ">
+            <div className="  h-[600px] mt-20  bg-cover bg-center bg-no-repeat bg-[url('/images/bgbanner.jpg')] overflow-hidden">
 
 
-                <div className="relative">
+                <div className='grid grid-cols-2'>
 
-
-                    <div className='relative'>
-
-
-                        <div className='h-screen absolute bg-white w-30 fullrotate right-80 top-0 p-4'>
-                            <div className='bg-red-300 w-full h-40 rounded-2xl -mt-20'>
-
-                            </div>
-
-                            <div className='bg-yellow-200 w-full h-38 rounded-2xl mt-5'>
-
-                            </div>
-
-                            <div className='bg-orange-300 w-full h-38 rounded-2xl mt-3'>
-
-                            </div>
-
-                            <div className='bg-yellow-200 w-full h-80 rounded-2xl mt-5'>
-
-                            </div>
-                        </div>
-
-                        <div
-                                className="w-80 h-70 rotate-12 absolute right-0 top-0 rounded-br-2xl  hover:scale-110 transition-all duration-300 cursor-pointer"
-                                style={{ clipPath: 'polygon(50% 100%, 0% 0%, 100% 0%)' }} >
-                                <Image
-                                    className="w-full h-full object-cover rounded-lg  shadow-lg "
-                                    src={'/mural/frame10.jpg'}
-                                    alt=""
-                                    width={500}
-                                    height={500}
-                                />
-                            </div>
-
-
-
-
-
-                        <div className="absolute right-40 top-0">
-
-                            <div
-                                className="w-60 h-30  absolute right-65 top-8 rounded-br-2xl  hover:scale-110 transition-all duration-300 cursor-pointer"
-                                style={{ clipPath: 'polygon(50% 100%, 0% 0%, 100% 0%)' }} >
-                                <Image
-                                    className="w-full h-full object-cover rounded-lg  shadow-lg "
-                                    src={'/mural/frame10.jpg'}
-                                    alt=""
-                                    width={500}
-                                    height={500}
-                                />
-                            </div>
-
-
-
-
-                            <div className="w-80 h-80  transform rotate-45 rounded-xl  relative overflow-visible">
-
-                                <div className="absolute top-2 left-2   transform  -rotate-45">
-                                    <div className="w-36 h-36 transform rotate-45 rounded-lg  hover:scale-110 transition-all duration-300 cursor-pointer overflow-hidden">
-
-                                        <Image
-                                            className="w-full h-full object-cover rounded-lg  shadow-lg "
-                                            src={'/mural/frame1.jpg'}
-                                            alt=""
-                                            width={500}
-                                            height={500}
-                                        />
-                                    </div>
-
-                                </div>
-
-                                <div className="absolute top-40 left-2 leading-0 -rotate-45">
-                                    <div className="w-36 h-36  transform rotate-45  hover:scale-110 transition-all duration-300 cursor-pointer">
-                                        <Image
-                                            className="w-full h-full object-cover rounded-lg  shadow-lg "
-                                            src={'/mural/frame2.jpg'}
-                                            alt=""
-                                            width={500}
-                                            height={500}
-                                        />
-                                    </div>
-                                </div>
-
-
-                                <div className="absolute right-2 top-19 transform -translate-y-1/2 -rotate-45">
-                                    <div className="w-36 h-36 bg-gradient-to-br transform rotate-45 hover:scale-110 transition-all duration-300 cursor-pointer">
-                                        <Image
-                                            className="w-full h-full object-cover rounded-lg  shadow-lg "
-                                            src={'/mural/frame4.jpg'}
-                                            alt=""
-                                            width={500}
-                                            height={500}
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="absolute -right-40 top-19 transform -translate-y-1/2 -rotate-45">
-                                    <div className="w-36 h-36  transform rotate-45 hover:scale-110 transition-all duration-300 cursor-pointer">
-                                        <Image
-                                            className="w-full h-full object-cover rounded-lg  shadow-lg "
-                                            src={'/mural/frame11.jpg'}
-                                            alt=""
-                                            width={500}
-                                            height={500}
-                                        />
-                                    </div>
-                                </div>
-
-
-                                <div className="absolute -bottom-16 -right-16 transform -translate-x-1/2 -translate-y-1/2 -rotate-45">
-                                    <div className="w-36 h-36 bg-gradient-to-br transform rotate-45 rounded-lg  hover:scale-110 transition-all duration-300 cursor-pointer">
-                                        <Image
-                                            className="w-full h-full object-cover rounded-lg  shadow-lg "
-                                            src={'/mural/frame3.jpg'}
-                                            alt=""
-                                            width={500}
-                                            height={500}
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="absolute -bottom-15 -right-57 transform -translate-x-1/2 -translate-y-1/2 -rotate-45">
-                                    <div className="w-36 h-36  transform rotate-45 rounded-lg hover:scale-110 transition-all duration-300 cursor-pointer">
-                                        <Image
-                                            className="w-full h-full object-cover rounded-lg  shadow-lg "
-                                            src={'/mural/frame6.png'}
-                                            alt=""
-                                            width={500}
-                                            height={500}
-                                        />
-                                    </div>
-                                </div>
-
-                                <div className="absolute -bottom-36 -right-[650px] transform -translate-x-1/2 -translate-y-1/2 -rotate-45">
-                                    <div className="w-80 h-80  transform rotate-45  hover:scale-110 transition-all duration-300 cursor-pointer">
-                                        <Image
-                                            className="w-full h-full object-cover rounded-lg shadow-lg"
-                                            src={'/mural/frame7.jpg'}
-                                            alt=""
-                                            width={500}
-                                            height={500}
-                                            style={{ objectPosition: '120% 40%' }}
-                                        />
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-
+                    <div className=''>
+                        <p>jhdsjkh</p>
                     </div>
 
+                    <div className="relative  h-full">
+                        <div className="absolute top-0 bottom-0 right-0 bg-white h-screen w-[600px] z-0"></div>
+
+
+                        <div className="absolute right-0 inset-0  z-10  w-ft h-screen">
+
+
+                            <div className='h-screen absolute w-25 fullrotate right-66 top-0 p-4 '>
+                                <div className='bg-white  w-full h-70 rounded-2xl -mt-60 '>
+                                    <Image
+                                        className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                        src={'/mural/lado1.jpg'}
+                                        alt=""
+                                        width={500}
+                                        height={500}
+                                    />
+                                </div>
+
+                                <div className=' w-full h-38 rounded-2xl mt-5'>
+                                    <Image
+                                        className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                        src={'/mural/lado2.jpg'}
+                                        alt=""
+                                        width={500}
+                                        height={500}
+                                    />
+                                </div>
+
+                                <div className='w-full h-38 rounded-2xl mt-3'>
+                                    <Image
+                                        className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                        src={'/mural/lado3.jpg'}
+                                        alt=""
+                                        width={500}
+                                        height={500}
+                                    />
+                                </div>
+
+                                <div className='w-full h-80 rounded-2xl mt-5'>
+                                    <Image
+                                        className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                        src={'/mural/lado4.jpg'}
+                                        alt=""
+                                        width={500}
+                                        height={500}
+                                    />
+
+                                </div>
+                            </div>
+
+
+                            <div
+                                className="w-125 h-75 -rotate-3 absolute -right-73 -top-2 rounded-br-2xl  hover:scale-110 transition-all duration-300 cursor-pointer"
+                                style={{ clipPath: 'polygon(50% 100%, 0% 0%, 100% 0%)' }} >
+                                <Image
+                                    className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                    src={'/mural/frame10.jpg'}
+                                    alt=""
+                                    width={500}
+                                    height={500}
+                                />
+                            </div>
+
+                            <div className="absolute right-40 top-0">
+
+                                <div
+                                    className="w-70 h-35  absolute right-60 top-0 rounded-br-2xl  hover:scale-110 transition-all duration-300 cursor-pointer"
+                                    style={{ clipPath: 'polygon(50% 100%, 0% 0%, 100% 0%)' }} >
+                                    <Image
+                                        className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                        src={'/mural/frame10.jpg'}
+                                        alt=""
+                                        width={500}
+                                        height={500}
+                                    />
+                                </div>
+
+
+                                <div className="w-80 h-80  transform rotate-45 rounded-xl  relative overflow-visible">
+
+                                    <div className="absolute top-2 left-2   transform  -rotate-45">
+                                        <div className="w-36 h-36 transform rotate-45 rounded-lg  hover:scale-110 transition-all duration-300 cursor-pointer overflow-hidden">
+
+                                            <Image
+                                                className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                                src={'/mural/frame1.jpg'}
+                                                alt=""
+                                                width={500}
+                                                height={500}
+                                            />
+                                        </div>
+
+                                    </div>
+
+                                    <div className="absolute top-40 left-2 leading-0 -rotate-45">
+                                        <div className="w-36 h-36  transform rotate-45  hover:scale-110 transition-all duration-300 cursor-pointer">
+                                            <Image
+                                                className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                                src={'/mural/frame2.jpg'}
+                                                alt=""
+                                                width={500}
+                                                height={500}
+                                            />
+                                        </div>
+                                    </div>
+
+
+                                    <div className="absolute right-2 top-19 transform -translate-y-1/2 -rotate-45">
+                                        <div className="w-36 h-36 bg-gradient-to-br transform rotate-45 hover:scale-110 transition-all duration-300 cursor-pointer">
+                                            <Image
+                                                className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                                src={'/mural/frame4.jpg'}
+                                                alt=""
+                                                width={500}
+                                                height={500}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="absolute -right-40 top-19 transform -translate-y-1/2 -rotate-45">
+                                        <div className="w-36 h-36  transform rotate-45 hover:scale-110 transition-all duration-300 cursor-pointer">
+                                            <Image
+                                                className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                                src={'/mural/frame11.jpg'}
+                                                alt=""
+                                                width={500}
+                                                height={500}
+                                            />
+                                        </div>
+                                    </div>
+
+
+                                    <div className="absolute -bottom-16 -right-16 transform -translate-x-1/2 -translate-y-1/2 -rotate-45">
+                                        <div className="w-36 h-36 bg-gradient-to-br transform rotate-45 rounded-lg  hover:scale-110 transition-all duration-300 cursor-pointer">
+                                            <Image
+                                                className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                                src={'/mural/frame3.jpg'}
+                                                alt=""
+                                                width={500}
+                                                height={500}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="absolute -bottom-15 -right-57 transform -translate-x-1/2 -translate-y-1/2 -rotate-45">
+                                        <div className="w-36 h-36  transform rotate-45 rounded-lg hover:scale-110 transition-all duration-300 cursor-pointer">
+                                            <Image
+                                                className="w-full h-full object-cover rounded-lg  shadow-lg "
+                                                src={'/mural/frame6.png'}
+                                                alt=""
+                                                width={500}
+                                                height={500}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="absolute -bottom-36 -right-[650px] transform -translate-x-1/2 -translate-y-1/2 -rotate-45">
+                                        <div className="w-80 h-80  transform rotate-45  hover:scale-110 transition-all duration-300 cursor-pointer">
+                                            <Image
+                                                className="w-full h-full object-cover rounded-lg shadow-lg"
+                                                src={'/mural/frame7.jpg'}
+                                                alt=""
+                                                width={500}
+                                                height={500}
+                                                style={{ objectPosition: '120% 40%' }}
+                                            />
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
                 </div>
+
+
             </div>
         </div>
 
